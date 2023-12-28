754d92fb5718311f878260a3115e473731288013
Sudhan <86077071+Sudhan-Dahake@users.noreply.github.com>
2023-12-27 21:51:55 -0500

    Delete Client/Client directory

--- Client/Client/authentication.h
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <fstream>
-#include <sstream>
-#include <string>
-#include <unordered_map>
-
-using namespace std;
-unordered_map<string, string> readCredentialsFile(void);
-void authenticate(unordered_map<string, string> umap);
